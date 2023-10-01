479816cab7c2505f90822837679841273727f3bc
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-10-01 19:40:20 +0530

    firebase configuration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,70 @@
-import React from 'react'
-import Header from './Header'
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData }from "../utils/validate"
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null)
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+
+     const message = checkValidData(email.current.value,password.current.value)
+     setErrorMessage(message);
+
+     // Sign / Sign up
+
+  };
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
-        <Header />
-        <form className='w-3/12 p-12 my-36 absolute mx-auto right-0 left-0 bg-black text-white rounded bg-opacity-80'>
-            <h1 className='font-bold text-3xl py-4'>Sign In</h1>
-            <input 
-            type="text" 
-            placeholder='Email Address' 
-            className="p-4 my-4 w-full bg-grey-700" 
-            />
-            <input 
-            type="password" 
-            placeholder='Password' 
-            className="p-4 my-4 w-full bg-grey-700" 
-            />
-            <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>Sign in</button>
-        </form>
-    </div> 
-  )
-}
+      <Header />
+      <form onSubmit = {(e) => e.preventDefault()}
+      className="w-3/12 p-12 my-36 absolute mx-auto right-0 left-0 bg-black rounded bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref= {name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-grey-700"
+          />
+        )}
+        <input
+          ref = {email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-grey-700"
+        />
 
-export default Login
+        <input
+          ref = {password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-grey-700"
+        />
+        <p className="text-red-500 py-2 text-lg font-bold">{ errorMessage }</p>
+
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg" 
+        onClick={handleButtonClick} >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-6 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAySahzkjkQAfAfiNZzACpo7otn0_cl5w4",
+  authDomain: "react-netflix-gpt.firebaseapp.com",
+  projectId: "react-netflix-gpt",
+  storageBucket: "react-netflix-gpt.appspot.com",
+  messagingSenderId: "99200240885",
+  appId: "1:99200240885:web:8affbe6ac5d239249b09bc",
+  measurementId: "G-65KEGFEKQ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
